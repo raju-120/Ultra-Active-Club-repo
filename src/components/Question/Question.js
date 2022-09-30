6ed1748a0e052f217b32ebdf89ed174981d2f8c3
Raju <ashfaqraju27@gmail.com>
2022-09-30 18:33:11 +0600

    question ans added

--- src/components/Question/Question.js
+++ src/components/Question/Question.js
@@ -1,9 +1,23 @@
 import React from 'react';
+import './Question.css'
 
 const Question = () => {
     return (
-        <div>
-            <h2>Question Part</h2>
+        <div className='question-container'>
+            <h2>Question Section </h2>
+            
+            <div>
+                <h3>How does React Works?</h3>
+                <p>ReactJS divides the UI into isolated reusable pieces of code known as components. React components work similarly to JavaScript functions as they accept arbitrary inputs called properties or props. It's possible to have as many components as necessary without cluttering your code.</p>
+            </div>
+            <div>
+                <h3>Difference between props and state?</h3>
+                <p>Props are used to pass data from one component to another. The state is a local data storage that is local to the component only and cannot be passed to other components. The this. setState property is used to update the state values in the component.</p>
+            </div>
+            <div>
+                <h3>What types of works useEffect does except data load?</h3>
+                <p>By using this Hook, you tell React that your component needs to do something after render. React will remember the function you passed (we'll refer to it as our “effect”), and call it later after performing the DOM updates.</p>
+            </div>
         </div>
     );
 };
